6e6d3c4ae3b8861f003bece90aece60006274fd9
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-05-27 19:13:58 -0300

    Arreglar parseado de equipamiento en las pruebas

--- tests/backend/un caso facilito/prueba.py
+++ tests/backend/un caso facilito/prueba.py
@@ -1,12 +1,19 @@
 import pandas as pd
 from asignacion_aulica import backend
 
-clases = pd.read_csv('clases.csv')
-aulas = pd.read_csv('aulas.csv')
+def parsear_equipamiento(literal):
+    componentes = (x.strip() for x in literal.split(','))
+    return {componente for componente in componentes if componente != ''}
+
+clases = pd.read_csv('clases.csv', keep_default_na=False)
+aulas = pd.read_csv('aulas.csv', keep_default_na=False)
+
+clases['equipamiento_necesario'] = list(map(parsear_equipamiento, clases['equipamiento_necesario']))
+aulas['equipamiento'] = list(map(parsear_equipamiento, aulas['equipamiento']))
 
 asignaciones = backend.asignar(aulas, clases)
 
-tabla_asignaciones = clases[['nombre', 'día', 'horario inicio', 'horario fin']].copy()
+tabla_asignaciones = clases[['nombre', 'día', 'horario_inicio', 'horario_fin']].copy()
 tabla_asignaciones['edificio'] = [aulas.loc[x, 'edificio'] for x in asignaciones]
 tabla_asignaciones['aula'] = [aulas.loc[x, 'nombre'] for x in asignaciones]
 

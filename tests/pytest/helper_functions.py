59805f6f3bd436fcc6f42a5bf5e32c8b9b24bc7b
Joaquín Rodríguez <133281891+IoachimusRoderici@users.noreply.github.com>
2025-06-02 15:51:16 -0300

    Docstring para predicado_es_not_equals_entre...

--- tests/pytest/helper_functions.py
+++ tests/pytest/helper_functions.py
@@ -47,13 +47,21 @@ def make_clases(n_aulas, *data):
     return clases, modelo
 
 def predicado_es_not_equals_entre_variable_y_constante(predicado, constante):
+    '''
+    Devuelve `True` si `predicado` es una expresión de la forma `variable != constante`,
+    donde `variable` es una variable de un `CpModel`.
+    '''
     return isinstance(predicado, cp_model.BoundedLinearExpression) \
         and len(predicado.vars) == 1 \
         and predicado.coeffs == [1] \
         and predicado.offset == 0 \
         and predicado.bounds.complement().flattened_intervals() == [constante, constante]
 
-def predicado_es_not_equals_entre__dos_variables(predicado):
+def predicado_es_not_equals_entre_dos_variables(predicado):
+    '''
+    Devuelve `True` si `predicado` es una expresión de la forma `variable1 != variable2`,
+    donde `variable1` y `variable2` son variables de un `CpModel`.
+    '''
     return isinstance(predicado, cp_model.BoundedLinearExpression) \
         and len(predicado.vars) == 2 \
         and predicado.coeffs == [1, -1] \

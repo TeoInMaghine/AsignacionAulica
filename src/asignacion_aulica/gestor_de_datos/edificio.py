ffef05c7ee61fede3daccfb39a997006458129f4
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-09-05 20:13:18 -0300

    agregar horario a los edificios

--- src/asignacion_aulica/gestor_de_datos/edificio.py
+++ src/asignacion_aulica/gestor_de_datos/edificio.py
@@ -5,3 +5,11 @@ class Edificio:
     nombre: str
     aulas_dobles: dict[str, tuple[str, str]] # Mapea el nombre del aula grande a los nombres de las aulas que la componen
     preferir_no_usar: bool = False # Indica que este edificio no es cómodo, y hay que asignarle pocas clases si es posible.
+
+    horario_lunes: Optional[tuple[time, time]] = None # Los horarios son tuplas (apretura, cierre). None si está cerrado ese día.
+    horario_martes: Optional[tuple[time, time]] = None
+    horario_miércoles: Optional[tuple[time, time]] = None
+    horario_jueves: Optional[tuple[time, time]] = None
+    horario_viernes: Optional[tuple[time, time]] = None
+    horario_sábado: Optional[tuple[time, time]] = None
+    horario_domingo: Optional[tuple[time, time]] = None

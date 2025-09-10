08cc7ded8c69c90f86047d41a3e8e61fd9a02094
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-09-10 18:53:02 -0300

    arreglar tipo de dato

--- src/asignacion_aulica/lógica_de_asignación/excepciones.py
+++ src/asignacion_aulica/lógica_de_asignación/excepciones.py
@@ -3,4 +3,4 @@ from asignacion_aulica.gestor_de_datos.día import Día
 class AsignaciónImposibleException(Exception):
     def __init__(self, *días_sin_asignar: Día):
         super().__init__()
-        self.días_sin_asignar: tuple[Día] = tuple(días_sin_asignar)
+        self.días_sin_asignar: tuple[Día, ...] = tuple(días_sin_asignar)

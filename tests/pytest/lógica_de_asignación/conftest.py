32aa53ca3729b30bee754fbeca1c15b7802aefcb
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-09-13 21:10:03 -0300

    Actualizar fixtures para usar las nuevas entidades

--- tests/pytest/lógica_de_asignación/conftest.py
+++ tests/pytest/lógica_de_asignación/conftest.py
@@ -6,70 +6,166 @@ prueba que lo necesita.
 
 Ver https://docs.pytest.org/en/stable/how-to/fixtures.html
 '''
+from collections.abc import Sequence
+from pandas import DataFrame
+from typing import Any
+import numpy as np
 import pytest
 
 from ortools.sat.python.cp_model import CpModel
-from pandas import DataFrame
-import numpy as np
+from datetime import time
 
-from asignacion_aulica.gestor_de_datos import Día
+from asignacion_aulica.gestor_de_datos.entidades import Edificio, Aula, Carrera, Materia, Clase
+from asignacion_aulica.gestor_de_datos.día import Día
 
 def pytest_configure(config):
     # Registrar los markers usados por las fixtures
+    config.addinivalue_line("markers", "edificios: marca para pasar parametros al fixture edificios")
     config.addinivalue_line("markers", "aulas: marca para pasar parametros al fixture aulas")
+    config.addinivalue_line("markers", "carreras: marca para pasar parametros al fixture carreras")
+    config.addinivalue_line("markers", "materias: marca para pasar parametros al fixture materias")
     config.addinivalue_line("markers", "clases: marca para pasar parametros al fixture clases")
     config.addinivalue_line("markers", "asignaciones_forzadas: marca para pasar parametros al fixture asignaciones")
 
-def make_aulas(aulas) -> DataFrame:
+def make_edificios(edificios: tuple[dict[str, Any], ...]) -> list[Edificio]:
     '''
-    Recibe datos de las aulas (posiblemente incompletos) y los rellena con valores por defecto.
+    Recibe datos de los edificios (posiblemente incompletos) y los rellena con
+    valores por defecto.
 
-    :param aulas: Tupla de diccionarios con datos (posiblemente incompletos) de aulas.
-    :return: Un DataFrame con el formato esperado por lógica_de_asignación.
+    :param edificios: Tupla de diccionarios con datos de edificios.
+    :return: La secuencia de edificios esperada por lógica_de_asignación.
     '''
-    default_values = {
-        'edificio': 'edificio',
-        'nombre': 'aula',
-        'capacidad': 1,
-        'equipamiento': set(),
-        'preferir_no_usar': False,
-        'horarios': {
-            Día.Lunes:     (0, 24),
-            Día.Martes:    (0, 24),
-            Día.Miércoles: (0, 24),
-            Día.Jueves:    (0, 24),
-            Día.Viernes:   (0, 24),
-            Día.Sábado:    (0, 24),
-            Día.Domingo:   (0, 24)
-        }
-    }
+    # Tiene que haber al menos un edificio
+    if len(edificios) == 0:
+        edificios = ({})
 
-    return DataFrame.from_records(default_values | explicit_values for explicit_values in aulas)
+    horario_default = (time(0), time(23, 59))
+    _edificios = [
+        Edificio(
+            edificio.get('nombre', f'edificio {i}'),
+            edificio.get('horario_lunes', horario_default),
+            edificio.get('horario_martes', horario_default),
+            edificio.get('horario_miércoles', horario_default),
+            edificio.get('horario_jueves', horario_default),
+            edificio.get('horario_viernes', horario_default),
+            edificio.get('horario_sábado', horario_default),
+            edificio.get('horario_domingo', horario_default),
+            edificio.get('aulas_dobles', dict()),
+            edificio.get('preferir_no_usar', False)
+        )
+        for i, edificio in enumerate(edificios)
+    ]
+    _edificios.sort(key=lambda e: e.nombre)
+    return _edificios
 
-def make_clases(clases) -> DataFrame:
+def make_aulas(aulas: tuple[dict[str, Any], ...]) -> list[Aula]:
     '''
-    Recibe datos de las clases (posiblemente incompletos) y los rellena con valores por defecto.
+    Recibe datos de las aulas (posiblemente incompletos) y los rellena con
+    valores por defecto.
 
-    :param clases: Tupla de diccionarios con datos (posiblemente incompletos) de clases.
-    :return: Un DataFrame con el formato esperado por lógica_de_asignación.
+    :param aulas: Tupla de diccionarios con datos de aulas.
+    :return: La secuencia de aulas esperada por lógica_de_asignación.
     '''
-    default_values = {
-        'nombre': 'materia',
-        'día': Día.Lunes,
-        'horario_inicio': 10,
-        'horario_fin': 11,
-        'cantidad_de_alumnos': 1,
-        'equipamiento_necesario': set(),
-        'edificio_preferido': 'edificio',
-        'aula_asignada': None
-    }
+    _aulas = [
+        Aula(
+            aula.get('nombre', f'aula {i}'),
+            aula.get('edificio', 'edificio 0'),
+            aula.get('capacidad', 1),
+            aula.get('equipamiento', set()),
+            aula.get('horario_lunes', None),
+            aula.get('horario_martes', None),
+            aula.get('horario_miércoles', None),
+            aula.get('horario_jueves', None),
+            aula.get('horario_viernes', None),
+            aula.get('horario_sábado', None),
+            aula.get('horario_domingo', None)
+        )
+        for i, aula in enumerate(aulas)
+    ]
+    _aulas.sort(key=lambda a: a.nombre)
+    _aulas.sort(key=lambda a: a.edificio)
+    return _aulas
 
-    data = {
-        columna: [clase.get(columna, default) for clase in clases]
-        for columna, default in default_values.items()
-    }
+def make_carreras(carreras: tuple[dict[str, Any], ...]) -> list[Carrera]:
+    '''
+    Recibe datos de las carreras (posiblemente incompletos) y los rellena con
+    valores por defecto.
 
-    return DataFrame(data, dtype=object)
+    :param aulas: Tupla de diccionarios con datos de carreras.
+    :return: La secuencia de carreras esperada por lógica_de_asignación.
+    '''
+    # Tiene que haber al menos una carrera
+    if len(carreras) == 0:
+        carreras = ({})
+
+    _carreras = [
+        Carrera(
+            carrera.get('nombre', f'carrera {i}'),
+            carrera.get('edificio_freferido', None)
+        )
+        for i, carrera in enumerate(carreras)
+    ]
+    _carreras.sort(key=lambda c: c.nombre)
+    return _carreras
+
+def make_materias(materias: tuple[dict[str, Any], ...]) -> list[Materia]:
+    '''
+    Recibe datos de las materias (posiblemente incompletos) y los rellena con
+    valores por defecto.
+
+    :param aulas: Tupla de diccionarios con datos de materias.
+    :return: La secuencia de materias esperada por lógica_de_asignación.
+    '''
+    # Tiene que haber al menos una materia
+    if len(materias) == 0:
+        materias = ({})
+
+    _materias = [
+        Materia(
+            materia.get('nombre', f'materia {i}'),
+            materia.get('carrera', 'carrera 0'),
+            materia.get('año', 1),
+            materia.get('cuatrimestral_o_anual', None)
+        )
+        for i, materia in enumerate(materias)
+    ]
+    _materias.sort(key=lambda c: c.nombre)
+    _materias.sort(key=lambda c: c.carrera)
+    return _materias
+
+def make_clases(clases: tuple[dict[str, Any], ...]) -> list[Clase]:
+    '''
+    Recibe datos de las clases (posiblemente incompletos) y los rellena con
+    valores por defecto.
+
+    :param clases: Tupla de diccionarios con datos de clases.
+    :return: La secuencia de clases esperada por lógica_de_asignación.
+    '''
+    _clases = [
+        Clase(
+            clase.get('id', i),
+            clase.get('materia', 'materia 0'),
+            clase.get('carrera', 'carrera 0'),
+            clase.get('día', Día.Lunes),
+            clase.get('horario_inicio', time(10)),
+            clase.get('horario_fin', time(11)),
+            clase.get('virtual', False),
+            clase.get('cantidad_de_alumnos', 1),
+            clase.get('equipamiento_necesario', set()),
+            clase.get('no_cambiar_asignación', False),
+            clase.get('edificio', None),
+            clase.get('aula', None),
+            clase.get('comisión', None),
+            clase.get('teórica_o_práctica', None),
+            clase.get('promocionable', None),
+            clase.get('docente', None),
+            clase.get('auxiliar', None)
+        )
+        for i, clase in enumerate(clases)
+    ]
+    _clases.sort(key=lambda c: c.materia)
+    _clases.sort(key=lambda c: c.carrera)
+    return _clases
 
 def make_asignaciones(
         clases: DataFrame,
@@ -108,27 +204,66 @@ def make_asignaciones(
     return asignaciones
 
 @pytest.fixture
-def aulas(request) -> DataFrame:
+def edificios(request: pytest.FixtureRequest) -> Sequence[Edificio]:
+    '''
+    Recibe en el marker "edificios" una tupla de diccionarios con datos
+    (posiblemente incompletos) de edificios.
+
+    Rellena esos datos con valores por defecto.
+
+    :return: La secuencia de edificios esperada por lógica_de_asignación.
+    '''
+    data = request.node.get_closest_marker('edificios').args
+    return make_edificios(data)
+
+@pytest.fixture
+def aulas(request) -> Sequence[Aula]:
     '''
     Recibe en el marker "aulas" una tupla de diccionarios con datos
     (posiblemente incompletos) de aulas.
 
     Rellena esos datos con valores por defecto.
 
-    :return: Un DataFrame con el formato esperado por lógica_de_asignación.
+    :return: La secuencia de aulas esperada por lógica_de_asignación.
     '''
     data = request.node.get_closest_marker('aulas').args
     return make_aulas(data)
 
 @pytest.fixture
-def clases(request) -> DataFrame:
+def carreras(request) -> Sequence[Carrera]:
+    '''
+    Recibe en el marker "carreras" una tupla de diccionarios con datos
+    (posiblemente incompletos) de carreras.
+
+    Rellena esos datos con valores por defecto.
+
+    :return: La secuencia de carreras esperada por lógica_de_asignación.
+    '''
+    data = request.node.get_closest_marker('carreras').args
+    return make_carreras(data)
+
+@pytest.fixture
+def materias(request) -> Sequence[Materia]:
+    '''
+    Recibe en el marker "materias" una tupla de diccionarios con datos
+    (posiblemente incompletos) de materias.
+
+    Rellena esos datos con valores por defecto.
+
+    :return: La secuencia de materias esperada por lógica_de_asignación.
+    '''
+    data = request.node.get_closest_marker('materias').args
+    return make_materias(data)
+
+@pytest.fixture
+def clases(request) -> Sequence[Clase]:
     '''
     Recibe en el marker "clases" una tupla de diccionarios con datos
     (posiblemente incompletos) de clases.
 
     Rellena esos datos con valores por defecto.
     
-    :return: Un DataFrame con el formato esperado por lógica_de_asignación.
+    :return: La secuencia de clases esperada por lógica_de_asignación.
     '''
     clases = request.node.get_closest_marker('clases').args
     return make_clases(clases)
--- tests/pytest/lógica_de_asignación/test_aulas_preprocesadas.py
+++ tests/pytest/lógica_de_asignación/test_aulas_preprocesadas.py
@@ -1,38 +1,42 @@
 from datetime import time
 
-from asignacion_aulica.lógica_de_asignación import aulas_preprocesadas
-from asignacion_aulica.lógica_de_asignación.aulas_preprocesadas import preprocesar_aulas, AulaPreprocesada
-from asignacion_aulica.gestor_de_datos import Edificio, Aula
+import pytest
 
-def test_un_solo_edificio():
-    edificios = [Edificio(
-        'nombre',
-        (time(8), time(20)),
-        (time(8), time(20)),
-        (time(8), time(20)),
-        (time(8), time(20)),
-        (time(8), time(20)),
-        (time(8), time(17)),
-        (time(0), time(0))
-    )]
-    aulas = [
-        Aula(
-            'una',
-            edificios[0].nombre,
-            25,
-            horario_lunes=(time(13), time(20)),
-            equipamiento={'qué equipazo'}
-        ),
-        Aula(
-            'otra',
-            edificios[0].nombre,
-            50,
-            horario_lunes=(time(13), time(20)),
-            horario_miércoles=(time(14), time(19)),
-            horario_viernes=(time(15), time(18)),
-            horario_domingo=(time(16), time(17))
-        )
-    ]
+from asignacion_aulica.lógica_de_asignación.aulas_preprocesadas import (
+    AulaPreprocesada,
+    preprocesar_aulas,
+    calcular_índices_de_aulas_dobles
+)
+
+@pytest.mark.edificios(dict(
+    nombre = 'nombre',
+    horario_lunes =     (time(8), time(20)),
+    horario_martes =    (time(8), time(20)),
+    horario_miércoles = (time(8), time(20)),
+    horario_jueves =    (time(8), time(20)),
+    horario_viernes =   (time(8), time(20)),
+    horario_sábado =    (time(8), time(17)),
+    horario_domingo =   (time(0), time(0))
+))
+@pytest.mark.aulas(
+    dict(
+        nombre='una',
+        edificio='nombre',
+        capacidad=25,
+        horario_lunes=(time(13), time(20)),
+        equipamiento={'qué equipazo'}
+    ),
+    dict(
+        nombre='y otra',
+        edificio='nombre',
+        capacidad=50,
+        horario_lunes=(time(13), time(20)),
+        horario_miércoles=(time(14), time(19)),
+        horario_viernes=(time(15), time(18)),
+        horario_domingo=(time(16), time(17))
+    )
+)
+def test_un_solo_edificio(edificios, aulas):
     aulas_preprocesadas_esperadas = [
         AulaPreprocesada(
             0,
@@ -52,59 +56,60 @@ def test_un_solo_edificio():
 
     assert preprocesar_aulas(edificios, aulas) == aulas_preprocesadas_esperadas
 
-def test_varios_edificios():
-    edificios = [
-        Edificio(
-            'nombre',
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(17)),
-            (time(0), time(0))
-        ),
-        Edificio(
-            'este no tiene ningún aula',
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(17)),
-            (time(0), time(0))
-        ),
-        Edificio(
-            'este si tiene aulas',
-            (time(15), time(20)),
-            (time(8, 30), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(20)),
-            (time(8), time(17)),
-            (time(0), time(0)),
-            preferir_no_usar=True
-        ),
-    ]
-    aulas = [
-        Aula(
-            'una',
-            edificios[0].nombre,
-            25,
-            horario_lunes=(time(13), time(20)),
-            equipamiento={'qué equipazo'}
-        ),
-        Aula(
-            'otra',
-            edificios[2].nombre,
-            50,
-        ),
-        Aula(
-            'otra más',
-            edificios[2].nombre,
-            31,
-        )
-    ]
+@pytest.mark.edificios(
+    dict(
+        nombre = 'Anasagasti 1',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0))
+    ),
+    dict(
+        nombre = 'Anasagasti 2',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0))
+    ),
+    dict(
+        nombre = 'Anasagasti 3',
+        horario_lunes =     (time(15), time(20)),
+        horario_martes =    (time(8, 30), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0)),
+        preferir_no_usar = True
+    )
+)
+@pytest.mark.aulas(
+    dict(
+        nombre='A102',
+        edificio='Anasagasti 1',
+        capacidad=25,
+        horario_lunes=(time(13), time(20)),
+        equipamiento={'qué equipazo'}
+    ),
+    # Ningún aula en Anasagasti 2
+    dict(
+        nombre='C306',
+        edificio='Anasagasti 3',
+        capacidad=50
+    ),
+    dict(
+        nombre='C345',
+        edificio='Anasagasti 3',
+        capacidad=31
+    )
+)
+def test_varios_edificios(edificios, aulas):
     aulas_preprocesadas_esperadas = [
         AulaPreprocesada(
             0,

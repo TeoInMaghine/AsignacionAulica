dc04f6ced5570f570ce3ecf2342d59932cd047bc
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-09-13 21:09:44 -0300

    calcular índices de aulas dobles

--- src/asignacion_aulica/lógica_de_asignación/aulas_preprocesadas.py
+++ src/asignacion_aulica/lógica_de_asignación/aulas_preprocesadas.py
@@ -1,8 +1,9 @@
+from bisect import bisect_left, bisect_right
 from collections.abc import Sequence
 from dataclasses import dataclass
 from datetime import time
 
-from asignacion_aulica.gestor_de_datos import Aula, Edificio
+from asignacion_aulica.gestor_de_datos.entidades import Aula, Edificio
 
 @dataclass
 class AulaPreprocesada:
@@ -53,6 +54,30 @@ def preprocesar_aulas(edificios: Sequence[Edificio], aulas: Sequence[Aula]) -> S
             i_aula += 1
 
     return aulas_preprocesadas
+
+def calcular_índices_de_aulas_dobles(edificios: Sequence[Edificio], aulas: Sequence[Aula]) -> dict[int, tuple[int, int]]:
+    '''
+    Juntar los diccionarios de aulas dobles de todos los edificios en uno solo,
+    y cambiar los nombres de aulas por sus índices.
+
+    :param edificios: Los edificios disponibles (ordenados alfabéticamente).
+    :param aulas: Las aulas disponibles en cada uno de los edificios (agrupadas
+    por edificio, en el mismo orden que la secuencia de edificios, y dentro de
+    cada edificio ordenadas alfabéticamente).
+    '''
+    aulas_dobles: dict[int, tuple[int, int]] = {}
+
+    for edificio in edificios:
+        i_min = bisect_left(aulas, edificio.nombre, key=lambda a: a.edificio)
+        i_max = bisect_right(aulas, edificio.nombre, lo=i_min, key=lambda a: a.edificio)
+        print(edificio.nombre, i_min, i_max)
+        for aula_grande, aulas_chicas in edificio.aulas_dobles.items():
+            i_aula_grande = bisect_left(aulas, aula_grande, lo=i_min, hi=i_max, key=lambda a: a.nombre)
+            i_aula_chica0 = bisect_left(aulas, aulas_chicas[0], lo=i_min, hi=i_max, key=lambda a: a.nombre)
+            i_aula_chica1 = bisect_left(aulas, aulas_chicas[1], lo=i_min, hi=i_max, key=lambda a: a.nombre)
+            aulas_dobles[i_aula_grande] = (i_aula_chica0, i_aula_chica1)
+
+    return aulas_dobles
 
 def _time_a_minutos(t: tuple[time, time]) -> tuple[int, int]:
     return t[0].minute + 60*t[0].hour, t[1].minute + 60*t[1].hour
--- tests/pytest/lógica_de_asignación/test_aulas_preprocesadas.py
+++ tests/pytest/lógica_de_asignación/test_aulas_preprocesadas.py
@@ -130,3 +130,117 @@ def test_varios_edificios():
     ]
 
     assert preprocesar_aulas(edificios, aulas) == aulas_preprocesadas_esperadas
+
+@pytest.mark.edificios(
+    dict(
+        nombre = 'Anasagasti 1',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0))
+    ),
+    dict(
+        nombre = 'Anasagasti 2',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0))
+    ),
+    dict(
+        nombre = 'Anasagasti 3',
+        horario_lunes =     (time(15), time(20)),
+        horario_martes =    (time(8, 30), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0)),
+        preferir_no_usar = True
+    )
+)
+@pytest.mark.aulas(
+    dict(
+        nombre='A102',
+        edificio='Anasagasti 1',
+        capacidad=25,
+        horario_lunes=(time(13), time(20)),
+        equipamiento={'qué equipazo'}
+    ),
+    # Ningún aula en Anasagasti 2
+    dict(
+        nombre='C306',
+        edificio='Anasagasti 3',
+        capacidad=50
+    ),
+    dict(
+        nombre='C345',
+        edificio='Anasagasti 3',
+        capacidad=31
+    )
+)
+def test_aulas_dobles_ninguna(edificios, aulas):
+    assert calcular_índices_de_aulas_dobles(edificios, aulas) == {}
+
+@pytest.mark.edificios(
+    dict(
+        nombre = 'Anasagasti 1',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0)),
+        aulas_dobles = {'A102': ('A102A', 'A102B')}
+    ),
+    dict(
+        nombre = 'Anasagasti 2',
+        horario_lunes =     (time(8), time(20)),
+        horario_martes =    (time(8), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0)),
+        aulas_dobles = {'B102': ('B102A', 'B102B'), 'B202': ('B202B', 'B202D')}
+    ),
+    dict(
+        nombre = 'Anasagasti 3',
+        horario_lunes =     (time(15), time(20)),
+        horario_martes =    (time(8, 30), time(20)),
+        horario_miércoles = (time(8), time(20)),
+        horario_jueves =    (time(8), time(20)),
+        horario_viernes =   (time(8), time(20)),
+        horario_sábado =    (time(8), time(17)),
+        horario_domingo =   (time(0), time(0)),
+        preferir_no_usar = True
+    )
+)
+@pytest.mark.aulas(
+    dict(nombre='A102', edificio='Anasagasti 1'),
+    dict(nombre='A102A', edificio='Anasagasti 1'),
+    dict(nombre='A102B', edificio='Anasagasti 1'),
+    dict(nombre='A103', edificio='Anasagasti 1'),
+    dict(nombre='B102', edificio='Anasagasti 2'),
+    dict(nombre='B102A', edificio='Anasagasti 2'),
+    dict(nombre='B102B', edificio='Anasagasti 2'),
+    dict(nombre='B103', edificio='Anasagasti 2'),
+    dict(nombre='B202', edificio='Anasagasti 2'),
+    dict(nombre='B202A', edificio='Anasagasti 2'),
+    dict(nombre='B202B', edificio='Anasagasti 2'),
+    dict(nombre='B202C', edificio='Anasagasti 2'),
+    dict(nombre='B202D', edificio='Anasagasti 2'),
+    dict(nombre='B202', edificio='Anasagasti 3'), # No son aulas dobles, pero tienen los mismos nombres que en el otro edificio
+    dict(nombre='B202A', edificio='Anasagasti 3'),
+    dict(nombre='B202B', edificio='Anasagasti 3')
+)
+def test_aulas_dobles_en_varios_edificios(edificios, aulas):
+    assert calcular_índices_de_aulas_dobles(edificios, aulas) == {
+        0: (1, 2), 4: (5, 6), 8: (10, 12)
+    }

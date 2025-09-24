2aaa1cca25192fa5c95394b62bdf6d1a368c192a
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-09-24 17:06:13 -0300

    usar nuevo preprocesamiento en restricciones

--- src/asignacion_aulica/lógica_de_asignación/restricciones.py
+++ src/asignacion_aulica/lógica_de_asignación/restricciones.py
@@ -1,17 +1,16 @@
 '''
 En este módulo se definen las restricciones del sistema de asignación.
 
-Las restricciones son condiciones rígidas que tiene que cumplir la asignación.
-Para simplificar la representación de los datos, se procesan primero las
-restricciones que determinan un valor fijo para una variable de asignación, y
-luego las restricciones que requieren comparar variables entre sí.
+Las restricciones son condiciones rígidas que tiene que cumplir la asignación de
+aulas para ser válida.
 
-Las restricciones del primer tipo se definen como funciones que toman los datos
-de clases y aulas, e indican qué valores se deben fijar.
+Hay algunas restricciones que se pueden calcular conociendo solamente los datos
+de las clases y los datos de las aulas, y que permiten anular algunas variables
+del modelo. Estas restricciones se calculan con la función `aulas_prohibidas`.
 
-Las del segundo tipo se definen como funciones que toman los datos de clases y
-aulas, y la matriz de asignaciones, y devuelven predicados que se deben agregar
-al modelo.
+Hay otras restricciones que para calcularla se necesita comparar las variables
+del modelo. Estas restricciones se calculan con la función
+`restricciones_con_variables`.
 
 Estas funciones toman los siguientes argumentos:
 - clases: Los datos de las clases en el problema de asignación.
@@ -22,58 +21,110 @@ Estas funciones toman los siguientes argumentos:
 
 Esto se omite de los docstrings para no tener que repetirlo en todos lados.
 '''
-from datetime import time
-from itertools import combinations, product
-from pandas import DataFrame
+from ortools.sat.python.cp_model import BoundedLinearExpression
+from itertools import combinations, product, chain
 from collections.abc import Iterable, Sequence
+from typing import Callable, TypeAlias
 import numpy as np
 
-from asignacion_aulica.gestor_de_datos.día import Día
 from asignacion_aulica.gestor_de_datos.entidades import Clase
-from asignacion_aulica.lógica_de_asignación.preprocesamiento import AulaPreprocesada
+from asignacion_aulica.lógica_de_asignación.preprocesamiento import (
+    AulaPreprocesada, AulasPreprocesadas, ClasesPreprocesadas
+)
+
+restricción_de_aulas_prohibidas: TypeAlias = Callable[
+    [ClasesPreprocesadas, AulasPreprocesadas],
+    Iterable[tuple[int, int]]
+]
+'''
+Las restricciones de aulas prohibidas se representan con funciones que reciben:
+- Los datos de las clases de el problema de asignación.
+- Los datos de las aulas disponibles.
+
+Y devuelven un iterable de tuplas `(clase, aula)` que representan las
+combinaciones de índice de clase e índice de aula que hay que evitar.
+'''
+
+restricción_con_variables: TypeAlias = Callable[
+    [ClasesPreprocesadas, AulasPreprocesadas, np.ndarray],
+    Iterable[BoundedLinearExpression]
+]
+'''
+Las restricciones con variables se representan con funciones que reciben:
+- Los datos de las clases de el problema de asignación.
+- Los datos de las aulas disponibles.
+- La matriz de variables de asignación, donde cada fila es una clase y cada
+  columna es un aula.
+
+Y devuelven un iterable de predicados que hay que agregar al modelo.
+'''
 
 def no_superponer_clases(
-    clases: Sequence[Clase],
-    aulas: Sequence[AulaPreprocesada],
-    aulas_dobles: dict[ int, tuple[int, int] ],
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas,
     asignaciones: np.ndarray
-):
+) -> Iterable[BoundedLinearExpression]:
     '''
     Las materias con horarios superpuestos no pueden estar en el mismo aula.
     '''
-    for i_clase1, clase1, i_clase2, clase2 in _pares_de_clases_que_se_superponen(clases):
-        for i_aula in range(len(aulas)):
-            yield asignaciones[i_clase1, i_aula] + asignaciones[i_clase2, i_aula] <= 1
+    for i_clase1, i_clase2 in _pares_de_clases_que_se_superponen(clases):
+        asignaciones_de_ambas_clases = asignaciones[i_clase1, :] + asignaciones[i_clase2, :]
+        for asignaciones_a_un_aula in asignaciones_de_ambas_clases:
+            yield asignaciones_a_un_aula <= 1
 
 def no_asignar_aula_doble_y_sus_hijas_al_mismo_tiempo(
-    clases: Sequence[Clase],
-    aulas: Sequence[AulaPreprocesada],
-    aulas_dobles: dict[ int, tuple[int, int] ],
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas,
     asignaciones: np.ndarray
-):
+) -> Iterable[BoundedLinearExpression]:
     '''
-    Si se asigna un aula doble en cierto horario, no pueden asignarse las aulas
-    individuales que la conforman a clases que se superpongan en ese horario.
+    Si se asigna un aula doble, las aulas que la conforman no pueden asignarse
+    a otras clases que en ese horario.
     '''
-    for i_clase1, clase1, i_clase2, clase2 in _pares_de_clases_que_se_superponen(clases):
-        for aula_doble, aulas_hijas in aulas_dobles.items():
+    for i_clase1, i_clase2 in _pares_de_clases_que_se_superponen(clases):
+        for aula_doble, aulas_hijas in aulas.aulas_dobles.items():
             # Se asigna una clase al aula doble => No se asigna ninguna clase a las aulas hijas 
             # Se asigna alguna clase a una de las aulas hijas => No se asigna una clase al aula doble
             for aula_hija in aulas_hijas:
                 yield asignaciones[i_clase1, aula_doble] + asignaciones[i_clase2, aula_hija] <= 1
                 yield asignaciones[i_clase2, aula_doble] + asignaciones[i_clase1, aula_hija] <= 1
 
-def no_asignar_en_aula_cerrada(clases: Sequence[Clase], aulas: Sequence[AulaPreprocesada]) -> Iterable[ tuple[int, int] ]:
+def no_asignar_aulas_ocupadas(
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas
+) -> Iterable[tuple[int, int]]:
     '''
-    La clase no puede estar en un aula que no esté abierta en ese horario.
+    Una clase no puede ser asignada a un aula que está ocupada en ese horario.
+    '''
+    for clase_e_índice, aula_ocupada in product(enumerate(clases.clases), clases.aulas_ocupadas):
+        i_clase, clase = clase_e_índice
+        i_aula, inicio, fin = aula_ocupada
+        se_superponen = clase.horario_inicio < fin and \
+                        clase.horario_fin > inicio
+        if se_superponen:
+            yield (i_clase, i_aula)
+            if i_aula in aulas.aulas_dobles:
+                aula1, aula2 = aulas.aulas_dobles[i_aula]
+                yield (i_clase, aula1)
+                yield (i_clase, aula2)
+
+def no_asignar_en_aula_cerrada(
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas
+) -> Iterable[tuple[int, int]]:
+    '''
+    Una clase no se puede asignar a un aula que no esté abierta en ese horario.
     '''
     for i_clase, clase, i_aula, aula in _combinaciones_de_clases_y_aulas(clases, aulas):
         aula_abre, aula_cierra = aula.horarios[clase.día]
-        aula_cerrada = aula_abre > clase.horario_inicio or aula_cierra < clase.horario_fin
-        if aula_cerrada:
+        aula_está_cerrada = aula_abre > clase.horario_inicio or aula_cierra < clase.horario_fin
+        if aula_está_cerrada:
             yield (i_clase, i_aula)
 
-def asignar_aulas_con_el_equipamiento_requerido(clases: Sequence[Clase], aulas: Sequence[AulaPreprocesada]) -> Iterable[ tuple[int, int] ]:
+def asignar_aulas_con_el_equipamiento_requerido(
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas
+) -> Iterable[tuple[int, int]]:
     '''
     Una clase no puede ser asignada a un aula que no tenga todo el equipamiento
     requerido.
@@ -82,103 +133,76 @@ def asignar_aulas_con_el_equipamiento_requerido(clases: Sequence[Clase], aulas: 
         if not clase.equipamiento_necesario.issubset(aula.equipamiento):
             yield (i_clase, i_aula)
 
-def no_asignar_aulas_ocupadas(
-    clases: Sequence[Clase],
-    aulas: Sequence[AulaPreprocesada],
-    aulas_dobles: dict[ int, tuple[int, int] ],
-    aulas_ocupadas: set[tuple[int, Día, time, time]]
-) -> Iterable[ tuple[int, int] ]:
-    '''
-    Una clase no puede ser asignada a un aula que está ocupada en ese horario.
-
-    Esta función recibe argumentos distintos que las otras, así que no está en
-    funciones_de_restricciones_de_aulas_prohibidas.
-    '''
-    for clase_e_índice, aula_ocupada in product(enumerate(clases), aulas_ocupadas):
-        i_clase, clase = clase_e_índice
-        aula, día, inicio, fin = aula_ocupada
-        se_superponen = clase.día == día and \
-                        clase.horario_inicio < fin and \
-                        clase.horario_fin > inicio
-        if se_superponen:
-            yield (i_clase, aula)
-            if aula in aulas_dobles:
-                yield (i_clase, aulas_dobles[aula][0])
-                yield (i_clase, aulas_dobles[aula][1])
-
-funciones_de_restricciones_de_aulas_prohibidas = (
+todas_las_restricciones_de_aulas_prohibidas: Sequence[restricción_de_aulas_prohibidas] = (
+    no_asignar_aulas_ocupadas,
     no_asignar_en_aula_cerrada,
     asignar_aulas_con_el_equipamiento_requerido
 )
 
-funciones_de_restricciones_con_variables = (
+todas_las_restricciones_con_variables: Sequence[restricción_con_variables] = (
     no_superponer_clases,
     no_asignar_aula_doble_y_sus_hijas_al_mismo_tiempo
 )
 
 def aulas_prohibidas(
-    clases: Sequence[Clase],
-    aulas: Sequence[AulaPreprocesada],
-    aulas_dobles: dict[ int, tuple[int, int] ],
-    aulas_ocupadas: set[tuple[int, Día, time, time]]
-    ) -> Iterable[ tuple[int, int] ]:
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas
+) -> Iterable[tuple[int, int]]:
     '''
-    Genera las combinaciones de clases y aulas que no pueden ser asignadas entre sí.
+    Genera las combinaciones de clases y aulas que no pueden ser asignadas entre
+    sí.
 
-    :param clases: Tabla con los datos de las clases.
-    :param aulas: Tabla con los datos de las aulas.
-    :param aulas_dobles: Diccionario donde las keys son los índices de las
-        aulas dobles y los valores son tuplas con las aulas individuales que
-        conforman el aula doble.
-    :param aulas_ocupadas: Tuplas (aula, día, inicio, fin) de momentos en los
-        que no se pueden usar algunas aulas.
-    :return: Iterable de tuplas (clase, aula), representando índices de la
-    matriz de asignaciones.
+    :param clases: Los datos de las clases de el problema de asignación.
+    :pram aulas: Los datos de las aulas disponibles.
+
+    :return: Iterable de tuplas (clase, aula) que representan las combinaciones
+    de índice de clase e índice de aula que hay que evitar.
     '''
-    for restricción in funciones_de_restricciones_de_aulas_prohibidas:
-        yield from restricción(clases, aulas)
-
-    yield from no_asignar_aulas_ocupadas(clases, aulas, aulas_dobles, aulas_ocupadas)
+    return chain.from_iterable(
+        restricción(clases, aulas)
+        for restricción in todas_las_restricciones_de_aulas_prohibidas
+    )
 
 def restricciones_con_variables(
-    clases: Sequence[Clase],
-    aulas: Sequence[AulaPreprocesada],
-    aulas_dobles: dict[ int, tuple[int, int] ],
-    asignaciones: np.ndarray) -> Iterable:
+    clases: ClasesPreprocesadas,
+    aulas: AulasPreprocesadas,
+    asignaciones: np.ndarray
+) -> Iterable[BoundedLinearExpression]:
     '''
-    :param clases: Los datos de las clases en el problema de asignación.
-    :param aulas: Los datos de todas las aulas disponibles.
-    :param aulas_dobles: Diccionario con los índices de las aulas dobles.
-    asignaciones: Matriz con los datos de asignaciones, donde las filas son
-    clases y las columnas son aulas.
+    :param clases: Los datos de las clases de el problema de asignación.
+    :pram aulas: Los datos de las aulas disponibles.
+    :param asignaciones: Matriz con las variables de asignaciones, donde las
+    filas son clases y las columnas son aulas.
 
     :return: Iterable de predicados que deben ser agregados al modelo.
     '''
-    for restricción in funciones_de_restricciones_con_variables:
-        yield from restricción(clases, aulas, aulas_dobles, asignaciones)
+    return chain.from_iterable(
+        restricción(clases, aulas, asignaciones)
+        for restricción in todas_las_restricciones_con_variables
+    )
 
-def _combinaciones_de_clases_y_aulas(clases: Sequence[Clase], aulas: Sequence[AulaPreprocesada]) -> Iterable[tuple[int, Clase, int, AulaPreprocesada]]:
+def _combinaciones_de_clases_y_aulas(clases: ClasesPreprocesadas, aulas: AulasPreprocesadas) -> Iterable[tuple[int, Clase, int, AulaPreprocesada]]:
     '''
     Devuelve un iterable de todas las combinaciones de clases y aulas.
 
     El iterable produce tuplas (índice de la clase, clase, índice del aula, aula).
     '''
-    for clase_con_índice, aula_con_ìndice in product(enumerate(clases), enumerate(aulas)):
+    for clase_con_índice, aula_con_ìndice in product(enumerate(clases.clases), enumerate(aulas.aulas)):
         i_clase, clase = clase_con_índice
         i_aula, aula = aula_con_ìndice
         yield i_clase, clase, i_aula, aula
 
-def _pares_de_clases_que_se_superponen(clases: Sequence[Clase]) -> Iterable[tuple[int, Clase, int, Clase]]:
+def _pares_de_clases_que_se_superponen(clases: ClasesPreprocesadas) -> Iterable[tuple[int, int]]:
     '''
     Devuelve un iterable de todos los pares de clases que se superponen entre sí.
 
-    El iterable produce tuplas (índice 1, clase 1, índice 2, clase 2).
+    El iterable produce tuplas (índice 1, índice 2).
     '''
-    for clase1_e_índice, clase2_e_índice in combinations(enumerate(clases), 2):
+    for clase1_e_índice, clase2_e_índice in combinations(enumerate(clases.clases), 2):
         i_clase1, clase1 = clase1_e_índice
         i_clase2, clase2 = clase2_e_índice
         se_superpopnen = clase1.día == clase2.día and \
            clase1.horario_inicio < clase2.horario_fin and \
            clase2.horario_inicio < clase1.horario_fin
         if se_superpopnen:
-            yield i_clase1, clase1, i_clase2, clase2
+            yield i_clase1, i_clase2
--- tests/pytest/lógica_de_asignación/test_restricciones.py
+++ tests/pytest/lógica_de_asignación/test_restricciones.py
@@ -7,12 +7,13 @@ from asignacion_aulica.gestor_de_datos.día import Día
 
 @pytest.mark.aulas({})
 @pytest.mark.clases(
-        dict(horario_inicio=1, horario_fin=3),
-        dict(horario_inicio=2, horario_fin=4),
-        dict(horario_inicio=5, horario_fin=6)
+        dict(día=Día.Lunes, horario_inicio=1, horario_fin=3),
+        dict(día=Día.Lunes, horario_inicio=2, horario_fin=4),
+        dict(día=Día.Lunes, horario_inicio=5, horario_fin=6)
     )
-def test_superposición(aulas_preprocesadas, clases, asignaciones):
-    predicados = list(restricciones.no_superponer_clases(clases, aulas_preprocesadas, {}, asignaciones))
+def test_superposición(aulas_preprocesadas, clases_preprocesadas, asignaciones):
+    clases_lunes = clases_preprocesadas[Día.Lunes]
+    predicados = list(restricciones.no_superponer_clases(clases_lunes, aulas_preprocesadas, asignaciones))
 
     # Debería generar solamente un predicado entre las primeras dos clases
     assert len(predicados) == 1
@@ -30,8 +31,9 @@ def test_superposición(aulas_preprocesadas, clases, asignaciones):
     dict(horario_lunes=(time(11), time(12))), # Abre tarde y cierra temprano
     dict(horario_lunes=(time( 0), time( 0))), # No abre los lunes
 )
-def test_aulas_cerradas(clases, aulas_preprocesadas):
-    prohibidas = list(restricciones.no_asignar_en_aula_cerrada(clases, aulas_preprocesadas))
+def test_aulas_cerradas(clases_preprocesadas, aulas_preprocesadas):
+    clases_lunes = clases_preprocesadas[Día.Lunes]
+    prohibidas = list(restricciones.no_asignar_en_aula_cerrada(clases_lunes, aulas_preprocesadas))
 
     # Debería generar restricciones con las aulas 1, 2, 4 y 5
     assert len(prohibidas) == 4
@@ -46,32 +48,35 @@ def test_aulas_cerradas(clases, aulas_preprocesadas):
     dict(equipamiento = set(('proyector', 'otra cosa'))),
     dict(equipamiento = set())
 )
-def test_equipamiento(clases, aulas_preprocesadas):
-    prohibidas = list(restricciones.asignar_aulas_con_el_equipamiento_requerido(clases, aulas_preprocesadas))
+def test_equipamiento(clases_preprocesadas, aulas_preprocesadas):
+    clases_lunes = clases_preprocesadas[Día.Lunes]
+    prohibidas = list(restricciones.asignar_aulas_con_el_equipamiento_requerido(clases_lunes, aulas_preprocesadas))
 
     # Debería generar una sola restricción con el aula 2
     assert len(prohibidas) == 1
     assert (0, 2) in prohibidas
 
+@pytest.mark.edificios(
+    dict(nombre='edificio 0', aulas_dobles={'102': ('102A', '102B')})
+)
 @pytest.mark.aulas(
-    dict(nombre = '102A'),
-    dict(nombre = '102B'),
-    dict(nombre = '102')
+    dict(edificio='edificio 0', nombre = '102'),
+    dict(edificio='edificio 0', nombre = '102A'),
+    dict(edificio='edificio 0', nombre = '102B')
 )
 @pytest.mark.clases(
-    dict(nombre = 'Clase 1'),
-    dict(nombre = 'Clase 2')
+    dict(día=Día.Lunes, nombre = 'Clase 1'),
+    dict(día=Día.Lunes, nombre = 'Clase 2')
 )
-def test_aulas_dobles(aulas_preprocesadas, clases, asignaciones): 
-    # 102 es un aula doble conformada por 102A y 102B
-    aulas_dobles = { 2: (0, 1) }
-
-    predicados = list(restricciones.no_asignar_aula_doble_y_sus_hijas_al_mismo_tiempo(clases, aulas_preprocesadas, aulas_dobles, asignaciones))
+def test_aulas_dobles(aulas_preprocesadas, clases_preprocesadas, asignaciones):
+    clases_lunes = clases_preprocesadas[Día.Lunes]
+    predicados = list(restricciones.no_asignar_aula_doble_y_sus_hijas_al_mismo_tiempo(clases_lunes, aulas_preprocesadas, asignaciones))
 
     assert len(predicados) == 4
     for predicado in predicados:
         assert predicado_es_nand_entre_dos_variables_bool(predicado)
-    assert any(asignaciones[0, 2] in predicado.vars and asignaciones[1, 0] in predicado.vars  for predicado in predicados)
-    assert any(asignaciones[0, 2] in predicado.vars and asignaciones[1, 1] in predicado.vars  for predicado in predicados)
-    assert any(asignaciones[1, 2] in predicado.vars and asignaciones[0, 0] in predicado.vars  for predicado in predicados)
-    assert any(asignaciones[1, 2] in predicado.vars and asignaciones[0, 1] in predicado.vars  for predicado in predicados)
+    
+    assert any(asignaciones[0, 0] in predicado.vars and asignaciones[1, 1] in predicado.vars  for predicado in predicados)
+    assert any(asignaciones[0, 0] in predicado.vars and asignaciones[1, 2] in predicado.vars  for predicado in predicados)
+    assert any(asignaciones[1, 0] in predicado.vars and asignaciones[0, 1] in predicado.vars  for predicado in predicados)
+    assert any(asignaciones[1, 0] in predicado.vars and asignaciones[0, 2] in predicado.vars  for predicado in predicados)

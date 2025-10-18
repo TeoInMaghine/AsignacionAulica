2c4ab843e6885d227292182d0efb55a2797595f2
IoachimusRoderici <joaq.rod0@gmail.com>
2025-10-18 16:07:31 -0300

    borrar imports no usados

--- src/asignacion_aulica/gestor_de_datos/gestor.py
+++ src/asignacion_aulica/gestor_de_datos/gestor.py
@@ -1,7 +1,6 @@
 from typing import Callable, Any
 
-from asignacion_aulica.gestor_de_datos.entidades import Aula, Edificio, Carrera, Materia, Clase
-from asignacion_aulica.lógica_de_asignación import AsignaciónImposibleException
+from asignacion_aulica.gestor_de_datos.entidades import Carrera
 
 class GestorDeDatos:
     '''

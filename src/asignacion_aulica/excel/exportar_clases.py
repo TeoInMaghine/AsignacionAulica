e57817998b4040d0638778776d480e7999708daa
Joaquín Rodríguez <133281891+IoachimusRoderici@users.noreply.github.com>
2026-02-03 16:55:04 -0300

    Typo
    Co-authored-by: TeoPG <59812116+TeoInMaghine@users.noreply.github.com>

--- src/asignacion_aulica/excel/exportar_clases.py
+++ src/asignacion_aulica/excel/exportar_clases.py
@@ -30,7 +30,7 @@ def exportar_datos_de_clases_a_excel(carreras: Sequence[Carrera], path: Path):
     Escribir los datos de las clases (incluyendo las aulas asignadas) en un
     archivo excel.
 
-    Cada carrera se exporta en una en una hoja del archivo.
+    Cada carrera se exporta en una hoja del archivo.
 
     :param carreras: Las carreras cuyos datos se quiere exportar.
     :param path: El path absoluto del archivo.
